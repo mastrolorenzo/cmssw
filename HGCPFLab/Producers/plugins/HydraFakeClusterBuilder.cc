e4c5a9e5c10e9a75edfe383e181c98b01222ca60
Luca Mastrolorenzo <luca.mastrolorenzo@cern.ch>
2016-02-16 23:31:16 +0100

    Add PFCluster collection to the output

--- HGCPFLab/Producers/plugins/HydraFakeClusterBuilder.cc
+++ HGCPFLab/Producers/plugins/HydraFakeClusterBuilder.cc
@@ -1,3 +1,5 @@
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Utilities/interface/InputTag.h"
@@ -20,10 +22,29 @@
 #include "DataFormats/ParticleFlowReco/interface/PFRecHitFraction.h"
 //#include "DataFormats/ForwardDetId/interface/HGCalDetId.h"
 
-//#include "RecoParticleFlow/PandoraTranslator/plugins/PandoraCMSPFCandProducer.h" // for CalibHGC
+// We probably don't need all of these
+#include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
+#include "Geometry/CaloGeometry/interface/TruncatedPyramid.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
+#include "Geometry/EcalAlgo/interface/EcalEndcapGeometry.h"
+#include "Geometry/EcalAlgo/interface/EcalBarrelGeometry.h"
+#include "Geometry/CaloTopology/interface/EcalEndcapTopology.h"
+#include "Geometry/CaloTopology/interface/EcalBarrelTopology.h"
+#include "Geometry/HcalTowerAlgo/interface/HcalGeometry.h"
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "Geometry/CaloGeometry/interface/FlatTrd.h"
+#include "DataFormats/ForwardDetId/interface/HGCalDetId.h"
+#include "DataFormats/ForwardDetId/interface/HGCEEDetId.h"
+#include "DataFormats/ForwardDetId/interface/HGCHEDetId.h"
+#include "Geometry/HGCalGeometry/interface/HGCalGeometry.h"
+#include "SimDataFormats/CaloTest/interface/HGCalTestNumbering.h"
 
+#include "TH2.h"
 using namespace std;
 using namespace edm;
+using namespace reco;
 
 class HydraFakeClusterBuilder : public EDProducer
 {
@@ -45,6 +66,22 @@ private:
     bool debugPrint_;
     double minDebugEnergy_;
 
+    unsigned int GetHGCLayer(const DetId& detid, const ForwardSubdetector& subdet) const;
+    
+    TH1F * h_ene_track;
+	TH1F * h_pt_track;
+	TH1F * h_eta_track;
+	TH1F * h_phi_track;
+	TH1F * h_X_track;
+	TH1F * h_Y_track;
+    TH1F * h_layer;
+    TH1F * h_det;
+    TH2D * h_cel_vs_X;
+    TH2D * h_cel_vs_Y;
+    TH2D * h_cel_vs_Z;
+    TH2D * h_cel_vs_XY;
+    
+
     //    CalibHGC m_calibEE, m_calibHEF, m_calibHEB;
     //    bool calibInitialized;
     // https://github.com/lgray/cmssw/blob/topic_cheated_reco/RecoParticleFlow/PandoraTranslator/plugins/PandoraCMSPFCandProducer.cc#L390-L477
@@ -58,7 +95,21 @@ HydraFakeClusterBuilder::HydraFakeClusterBuilder( const ParameterSet &iConfig ) 
     minDebugEnergy_( iConfig.getUntrackedParameter<double>( "MinDebugEnergy", 10. ) )
     //    m_calibEE(ForwardSubdetector::HGCEE,"EE",debugPrint), m_calibHEF(ForwardSubdetector::HGCHEF,"HEF",debugPrint), m_calibHEB(ForwardSubdetector::HGCHEB,"HEB",debugPrint), calibInitialized(false),
 {
+    edm::Service<TFileService> fs; 
     produces<reco::PFClusterCollection>();
+    h_ene_track = fs->make<TH1F>("h_ene_track","Energy of the track associated to cluster",1000,0,1000);
+    h_pt_track = fs->make<TH1F>("h_pt_track","Pt of the track associated to cluster",150,0,150);
+    h_eta_track = fs->make<TH1F>("h_eta_track","Eta of the track associated to cluster",500,-4,4);
+    h_phi_track = fs->make<TH1F>("h_phi_track","Phi of the track associated to cluster",500,-3.5,3.5);
+    h_X_track   = fs->make<TH1F>("h_X_track","X of the track associated to cluster",300,-150,150);
+    h_Y_track   = fs->make<TH1F>("h_Y_track","Y of the track associated to cluster",300,-150,150);
+	h_layer   = fs->make<TH1F>("h_layer","h_layer",50,0,50);
+    h_det   = fs->make<TH1F>("h_det","h_det",15,0,15);
+    h_cel_vs_X   = fs->make<TH2D>("h_cel_vs_X","h_cel_vs_X",300,-150,150,500,0,500);
+    h_cel_vs_Y   = fs->make<TH2D>("h_cel_vs_Y","h_cel_vs_Y",300,-150,150,500,0,500);
+    h_cel_vs_Z   = fs->make<TH2D>("h_cel_vs_Z","h_cel_vs_Z",820,-410,410,500,0,500);
+    h_cel_vs_XY	 = fs->make<TH2D>("h_cel_vs_XY","h_cel_vs_XY",300,-150,150,300,-150,150);
+
 }
 
 void HydraFakeClusterBuilder::produce( Event &iEvent, const EventSetup & )
@@ -110,13 +161,19 @@ void HydraFakeClusterBuilder::produce( Event &iEvent, const EventSetup & )
         auto simHits = hydraObj->simHitsFromSimTrack( i, true );
         for ( unsigned j = 0 ; j < hydraObj->recHitSize(); j++ ) {
             edm::Ptr<reco::PFRecHit> currentRecHit = hydraObj->recHit( j );
+            std::cout << " hasSimHitFromRecHit( j ) = "  << hydraObj->hasSimHitFromRecHit( j ) << std::endl;
             if ( hydraObj->hasSimHitFromRecHit( j ) ) {
                 if ( !splitRecHits_ ) {
                     auto result = hydraObj->simHitAndFractionFromRecHit( j );
+                    
+                    //Returns an iterator to the first element in the range [first,last) 
+                    //that compares equal to val. If no such element is found, the function returns last.
                     auto match = std::find(simHits.begin(), simHits.end(), result.first );
+                    
                     if ( match != simHits.end() ) {
-                        if ( false && debugPrint_ ) {
-                            std::cout << "   Track " << i << " matches to rechit " << j << " with fraction " << result.second;
+						std::cout << " match true! "<< std::endl;
+                        if ( true ) {
+                            std::cout << "   Track " << i << "  simHit "<<(result.first)->energy() << " matches to rechit " << j << " with fraction " << result.second;
                             std::cout << "     e(track) = " << currentTrack->momentum().E() << " e(hit)=" << currentRecHit->energy();
                             std::cout << std::endl;
                         }
@@ -125,6 +182,33 @@ void HydraFakeClusterBuilder::produce( Event &iEvent, const EventSetup & )
                         std::cout << "       Unsplit: we use a fraction of 1, but it's really " << result.second << std::endl;
                         reco::PFRecHitFraction rhf( currentRecHitRef, 1. ); // unsplit case
                         temp.addRecHitFraction( rhf );
+                        
+						int layer = 999;
+                        int det = 999;
+						
+                        if(true) std::cout <<  "recHit Id = " << currentRecHitRef->detId() << std::endl;
+						DetId hitid(currentRecHitRef->detId());
+						layer = GetHGCLayer( hitid, (ForwardSubdetector)hitid.subdetId() );
+                        det = hitid.subdetId();
+                        
+						if( det == 3 ){ 
+                        	layer = GetHGCLayer( hitid, (ForwardSubdetector)hitid.subdetId() );
+                        }else if( det == 4 ){
+                        	layer = 28 + GetHGCLayer( hitid, (ForwardSubdetector)hitid.subdetId() );
+                        }else if( det == 5 ){
+                        	layer = 28 + 12 + GetHGCLayer( hitid, (ForwardSubdetector)hitid.subdetId() );
+                        }
+						std::cout << "layer: " <<(unsigned int) ((HGCalDetId)(hitid)).layer(); //<< " waffer: " <<(unsigned int) ((HGCalDetId)(hitid)).sector();
+                        //std::cout << " cel type: " << (unsigned int) ((HGCalDetId)(hitid)).subsector()
+                        std::cout << " cell: " << (unsigned int) ((HGCalDetId)(hitid)).cell() << std::endl;
+						if(debugPrint_) std::cout << "recHit Id = "<< currentRecHitRef->detId() << "  subdet = " << det << "  layer = " << layer << std::endl;
+                        
+                        h_layer->Fill(layer);
+                        h_det->Fill(det);
+                        h_cel_vs_X->Fill(currentRecHitRef->position().x(), (unsigned int) ((HGCalDetId)(hitid)).cell());
+						h_cel_vs_Y->Fill(currentRecHitRef->position().y(), (unsigned int) ((HGCalDetId)(hitid)).cell());
+                        h_cel_vs_Z->Fill(currentRecHitRef->position().z(), (unsigned int) ((HGCalDetId)(hitid)).cell());
+						h_cel_vs_XY->Fill(currentRecHitRef->position().x(), currentRecHitRef->position().y(), (unsigned int) ((HGCalDetId)(hitid)).cell() ); 
                     }
                 } else {
                     auto result_vec = hydraObj->simHitsAndFractionsFromRecHit( j );
@@ -172,6 +256,13 @@ void HydraFakeClusterBuilder::produce( Event &iEvent, const EventSetup & )
                 if (debugPrint_) std::cout << "Track " << i << " phi=" << currentTrack->momentum().Phi() << " fake cluster phi=" << temp.phi() << std::endl;
             }
             pfClusterCol->push_back( temp );
+            h_ene_track->Fill(currentTrack->momentum().E());
+            h_pt_track->Fill(currentTrack->momentum().Pt());            
+            h_eta_track->Fill(currentTrack->momentum().Eta());
+            h_phi_track->Fill(currentTrack->momentum().Phi());
+            h_X_track->Fill(currentTrack->trackerSurfacePosition().X());
+            h_Y_track->Fill(currentTrack->trackerSurfacePosition().Y());
+            
         } else {
             if ( false ) std::cout << "   cluster has no hits, skipping" << std::endl;
         }
@@ -180,6 +271,16 @@ void HydraFakeClusterBuilder::produce( Event &iEvent, const EventSetup & )
 
     iEvent.put ( pfClusterCol );
 }
+
+
+unsigned int HydraFakeClusterBuilder::GetHGCLayer(const DetId& detid, const ForwardSubdetector& subdet) const {
+    unsigned int layer = 0;
+	
+    layer = (unsigned int) ((HGCalDetId)(detid)).layer() ;
+	
+    return layer;
+}
+
 
 DEFINE_FWK_MODULE( HydraFakeClusterBuilder );
 
--- HGCPFLab/Producers/test/testHydra_ReadOnly_cfg.py
+++ HGCPFLab/Producers/test/testHydra_ReadOnly_cfg.py
@@ -2,7 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('TESTHYDRA')
 
-process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('file:step3_HydraOnly.root'))
+#process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/l/lmastrol/CMSSW_8_0_X_2016-01-26-1100/src/HGCPFLab/Producers/test/step3_HydraOnly_Ele35.root'))
+#process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/l/lmastrol/LatestHydra/CMSSW_8_0_X_2016-02-07-2300/src/HGCPFLab/Producers/test/step3_HydraOnly_Photon35.root'))
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/l/lmastrol/LatestHydra/CMSSW_8_0_X_2016-02-07-2300/src/HGCPFLab/Producers/test/step3_HydraOnly_Mu100.root'))
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
 
 process.ExampleReader = cms.EDProducer("ExampleHydraPFProducer",HydraTag=cms.InputTag("Hydra"))
 
@@ -17,8 +22,25 @@ process.FakeClusterCaloFace = cms.EDProducer("HydraFakeClusterBuilder",HydraTag=
                                              UseGenParticles=cms.bool(False),
                                              MinDebugEnergy=cms.untracked.double(30.)
                                         )
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands =  cms.untracked.vstring('keep *'),
+    #fileName = cms.untracked.string('file:step3b_hydra_photons_1ev.root'),
+	fileName = cms.untracked.string('file:testMu100.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-RECO')
+    )
+)
 
+process.TFileService = cms.Service("TFileService",
+      fileName = cms.string("histoMu.root"),
+      closeFileFast = cms.untracked.bool(True)
+  )
+  
+  
 #process.testSequence = cms.Sequence(process.ExampleReader+process.FakeClusterGen+process.FakeClusterCaloFace)
-process.testSequence = cms.Sequence(process.FakeClusterGen+process.FakeClusterCaloFace)
-process.p = cms.Path(process.testSequence)
-
+process.testSequence = cms.Sequence(process.FakeClusterCaloFace)
+process.p  = cms.Path(process.testSequence)
+process.ep = cms.EndPath(process.RECOSIMoutput)
